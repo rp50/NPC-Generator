2ec4ff9c8069fbe26831e155d4e1ebfb1ab7d4dd
Robert Paul <rob.k.paul@gmail.com>
2021-01-29 22:55:01 -0800

    structure for storing NPCs during run time

--- main.cpp
+++ main.cpp
@@ -1,38 +1,24 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include "npc.h"
 
-int cmd_reader(std::string cmd){
-    int cmd_code = -1;
-    if(cmd == "new"){
-        cmd_code = 1;
-    }
-    else if(cmd.find("new") == 0){
-        cmd_code = 2;
-    }
-    else if(cmd == "exit"){
-        cmd_code = 0;
-    }
-    return cmd_code;
-}
+
 
 int main(){
     std::string cmd;
+    std::map<std::string, NPC> NPC_List;
     while(std::getline(std::cin, cmd)){
-        int cmd_code = cmd_reader(cmd);
-        switch (cmd_code){
-            case 0:
-                exit(0);
-                break;
-            case 1:
-            // request random new 
-                std::cout << "rand new" << std::endl;
-                break;
-            case 2:
-            // request new with either specified race / name
-                std::cout << "spec new" << std:: endl;
-                break;
-            default:
-                break;
+        if(cmd == "exit"){
+            exit(0);
+        }
+        else if(cmd == "new") {
+            NPC current_NPC;
+            current_NPC.print();
+            NPC_List.emplace(current_NPC.getName(), current_NPC);
+        }
+        else{
+            std::cout << "Unrecognized command \"" << cmd << "\", type help to see a list of commands." << std::endl;
         }
     }
     return 0;
